345ebaf8018c6725ac6443046cd7c4a2d76b82dc
Md Arif Hossen <arifsohag2500@gmail.com>
2024-08-21 22:43:47 +0600

    4

---
+++ app/Helper/JWTToken.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Helper;
+
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Exception;
+
+class JWTToken
+{
+
+    function CreateToken(string $userEmail): string
+    {
+        $key = env('JWT_KEY');
+        $payload = [
+            'iss' => 'laravel-token',
+            'iat' => time(),
+            'exp' => time() + 60 * 60, // Token expires in 1 hour
+            'userEmail' => $userEmail
+        ];
+
+        return JWT::encode($payload, $key, 'HS256');
+    }
+
+    function VerifyToken($token)
+    {
+        try {
+
+            $key = env('JWT_KEY');
+            $decode = JWT::decode($token,new Key($key,'HS256'));
+            return   $decode->userEmail;
+        }
+        catch(Exception $e)
+
+        {
+            return   'unauthorized';
+
+        }
+
+
+
+    }
+}
