1322903fbf94b8d6e978dea42d031b0f96b98d87
Md Arif Hossen <arifsohag2500@gmail.com>
2024-08-29 23:45:28 +0600

    19

--- app/Helper/JWTToken.php
+++ app/Helper/JWTToken.php
@@ -3,22 +3,24 @@
 
 namespace App\Helper;
 
-
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\SignatureInvalidException;
 use Exception;
 
 class JWTToken
 {
 
-    public static  function CreateToken(string $userEmail): string
+    public static  function CreateToken(string $userEmail,$userID): string
     {
         $key = env('JWT_KEY');
         $payload = [
             'iss' => 'laravel-token',
             'iat' => time(),
             'exp' => time() + 60 * 60, // Token expires in 1 hour
-            'userEmail' => $userEmail
+            'userEmail' => $userEmail,
+            'userID' => $userID
         ];
 
         return JWT::encode($payload, $key, 'HS256');
@@ -31,19 +33,25 @@ class JWTToken
             'iss' => 'laravel-token',
             'iat' => time(),
             'exp' => time() + 60 * 20, // Token expires in 1 hour
-            'userEmail' => $userEmail
+            'userEmail' => $userEmail,
+            'userID' => '0'
+
         ];
 
         return JWT::encode($payload, $key, 'HS256');
     }
 
-    public static function VerifyToken($token)
+    public static function VerifyToken($token):string|object
     {
         try {
+            if($token==null)
+            {
+                return 'unauthorized';
+            }
 
             $key = env('JWT_KEY');
             $decode = JWT::decode($token,new Key($key,'HS256'));
-            return   $decode->userEmail;
+            return   $decode;
         }
         catch(Exception $e)
 
--- app/Http/Middleware/TokenVerificationMiddleware.php
+++ app/Http/Middleware/TokenVerificationMiddleware.php
@@ -16,23 +16,18 @@ class TokenVerificationMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $token = $request->cookie('token');
-         $result =  JWTToken::VerifyToken($token);
 
-         if($result=='unauthorized')
-         {
+        $token=$request->cookie('token');
+        $result=JWTToken::VerifyToken($token);
+        if($result=="unauthorized"){
+            return redirect('/userLogin');
+        }
+        else{
+            $request->headers->set('email',$result->userEmail);
+            $request->headers->set('id',$result->userID);
+            return $next($request);
+        }
 
-             return response()->json([
-                 'status' => 'failed',
-                 'message' => 'unauthorized'
-             ],401);
-         }
-         else
-         {
-             $request->headers->set('email',$result);
-             return $next($request);
-         }
 
-        return $next($request);
     }
 }
